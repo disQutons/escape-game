92d7aebe5d78c61c1ff472e0c02a29619436d314
Muhammad Hassan <muhammadhassan@exceedgulf.com>
2023-02-01 09:54:01 +0500

    pattern lock implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
+
+
+declare var PatternLock: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'pattern-lock';
+  matchedPattern = 5784;
+  isMatched: boolean|null = null;
+
+  ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit(): void {
+    const me = this;
+    var p = new PatternLock(document.getElementById('lock'), {
+        onPattern: function(pattern:any){
+          if(pattern) {
+            if(me.matchedPattern === pattern) {
+              me.isMatched = true;
+              this.success()
+            } else {
+              me.isMatched = false;
+              this.error()
+            }
+          }
+          
+        }
+    });
+  }
+  
 }
