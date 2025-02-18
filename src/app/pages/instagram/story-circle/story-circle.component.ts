25e570a1f8f35b6e9e409801affb84f4954009e0
Blampied <lulublamp@hotmail.com>
2025-02-18 18:00:19 +0100

    feat: add fullscreen story display with StoryService

--- src/app/pages/instagram/story-circle/story-circle.component.ts
+++ src/app/pages/instagram/story-circle/story-circle.component.ts
@@ -1,13 +1,21 @@
-import {Component, Input} from '@angular/core';
-import {User} from "../../../models/story.model";
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { User } from '../../../models/story.model';
 
 @Component({
   selector: 'app-story-circle',
   templateUrl: './story-circle.component.html',
-  styleUrls: ['./story-circle.component.css']
+  styleUrls: ['./story-circle.component.css'],
 })
 export class StoryCircleComponent {
   @Input() story: User | undefined;
   @Input() size: number = 80;
   @Input() showAvatarCircle: boolean = true;
+
+  @Output() storyClicked = new EventEmitter<User>();
+
+  onClick() {
+    if (this.story) {
+      this.storyClicked.emit(this.story);
+    }
+  }
 }
